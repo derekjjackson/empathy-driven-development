39b7eaadfb636ce9b5d2d3c6f639cb87471f4fd2
Marcy Sutton <marcy.sutton@deque.com>
2018-06-26 15:58:53 -0400

    toggle animation via button/OS pref

--- src/App.js
+++ src/App.js
@@ -6,38 +6,61 @@ import {
 } from 'react-router-dom'
 import './App.css'
 
+import {AnimationContext} from './animation-context'
+
 import LoginFormDemo from './pages/login-form-demo'
 import CardFlipDemo from './pages/card-flip-demo'
 import MenuDemo from './pages/menu-demo'
 
 class App extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+        animationEnabled: true
+    }
+  }
+
+  handleAnimationPref() {
+    this.setState({
+      animationEnabled: !this.state.animationEnabled
+    })
+  }
+
   render() {
     return (
-      <Router>
-        <div className="App">
-          <header className="App-header" role="banner">
-            <h1 className="App-title">
-              <NavLink to="/">Component Library</NavLink>
-            </h1>
-            <ul className="Component-list">
-              <li><NavLink 
-                  to="/login-form"
-                  >Login Form</NavLink> <span></span></li>
+      <AnimationContext.Provider value={this.state.animationEnabled ? 'animationsOn' : ''}>
+        <Router>
+          <div className="App">
+            <header className="App-header" role="banner">
+              <h1 className="App-title">
+                <NavLink to="/">Component Library</NavLink>
+              </h1>
+              <ul className="Component-list">
                 <li><NavLink 
-                  to="/card-flip"
-                  >Card Flip</NavLink> <span></span></li>
-                <li><NavLink 
-                  to="/menu"
-                  >Menu</NavLink> <span></span></li>
-            </ul>
-          </header>
-          <main className="main" role="main">
-            <Route path="/login-form" component={LoginFormDemo}/>
-            <Route path="/card-flip" component={CardFlipDemo}/>
-            <Route path="/menu" component={MenuDemo}/>
-          </main>
-        </div>
-      </Router>
+                    to="/login-form"
+                    >Login Form</NavLink> <span></span></li>
+                  <li><NavLink 
+                    to="/card-flip"
+                    >Card Flip</NavLink> <span></span></li>
+                  <li><NavLink 
+                    to="/menu"
+                    >Menu</NavLink> <span></span></li>
+              </ul>
+              <button
+                className="Animation-pref"
+                onClick={this.handleAnimationPref.bind(this)}>
+                Turn {this.state.animationEnabled ? 'off' : 'on'} animations<br />
+              </button>
+            </header>
+            <main className="main" role="main">
+              <Route path="/login-form" component={LoginFormDemo}/>
+              <Route path="/card-flip" component={CardFlipDemo}/>
+              <Route path="/menu" component={MenuDemo}/>
+            </main>
+          </div>
+        </Router>
+      </AnimationContext.Provider>
     )
   }
 }
